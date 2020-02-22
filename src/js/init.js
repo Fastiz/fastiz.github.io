53ce682d85ba765e40a0e4d186905a8e45004466
fastiz <facuastiz@gmail.com>
2020-02-21 22:31:14 -0300

    added download button

--- src/js/init.js
+++ src/js/init.js
@@ -4,4 +4,14 @@ const HEIGHT = 400, WIDTH = 400;
 function init(){
     drawCanvasInit();
     modalInit();
+
+    document.getElementById('download-button').onclick = function(){
+        const canvas = document.getElementById("draw-shapes");
+        const image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
+        const link = document.createElement('a');
+        link.download = "generated-image.png";
+        link.href = image;
+        link.click();
+    };
+
 }
